0e9ef548e0867726eaab98cc0f1430808fe86ae1
paindespik <seandriard3@gmail.com>
2020-11-04 11:07:01 -0100

    inventaire

--- main.py
+++ main.py
@@ -8,27 +8,33 @@ class Personnage:
         self.height = 39
         self.x = 0
         self.y = 0
+        self.case = (0,0)
         self.hautGauche = (self.x, self.y)
         self.hautDroit = (self.x + self.width, self.y)
         self.basGauche = (self.x, self.y+self.height)
         self.basDroit = (self.x + self.width, self.y+self.height)
+        self.inventaire = []
         self.image = pygame.image.load('images/personnage.png')
 
     def move_right(self):
         if self.x+self.width + 5 +pas < width:
             self.x = self.x + pas
+            self.case = (int(self.x/50), int(self.y/50))
 
     def move_left(self):
         if self.x-pas > 0:
             self.x = self.x - pas
+            self.case = (int(self.x/50), int(self.y/50))
 
     def move_up(self):
         if self.y + 5 - pas > 0:
             self.y = self.y - pas
+            self.case = (int(self.x/50), int(self.y/50))
 
     def move_down(self):
         if self.y+pas < height:
             self.y = self.y + pas
+            self.case = (int(self.x/50), int(self.y/50))
 
 
 def drawGrid(w, h, lines, rows, surface):
@@ -37,9 +43,9 @@ def drawGrid(w, h, lines, rows, surface):
         for j in range(len(carte[0])):
             if carte[j][i] == 0:
                 surface.blit(pygame.image.load('images/route2.jpg'), (50*j, 50*i, 50, 50))
-                etatevenement =  random.randrange(0, 10 , 1)
-                if etatevenement ==1:
+                if (j,i) in listDechet:
                     surface.blit(pygame.image.load('images/canette1.png'), (50 * j, 50 * i, 50, 50))
+
             elif carte[j][i] == 1:
                 if i != 0:
                     if carte[j][i-1] == 0:
@@ -87,6 +93,29 @@ def texte(surface):
     txtTitre = "EcoTown"
     titre = font.render(txtTitre, True, (255, 255, 255))
     surface.blit(titre, (width+(widthTexte/3), 50))
+    if (int(personnage.x/50), int(personnage.y/50)) in listDechet:
+        txtTexte = "press SPACE to carry"
+        texte = font.render(txtTexte, True, (0, 255, 0))
+        surface.blit(texte, (width, 100))
+    if personnage.inventaire:
+        x = 0
+        y = 300
+        for l in range(int(len(personnage.inventaire)/3)+1):
+            pygame.draw.line(surface, (255, 255, 255), (width, y), (width+widthTexte, y))
+            pygame.draw.line(surface, (255, 255, 255), (width, y+50), (width+widthTexte, y+50))
+            pygame.draw.line(surface, (255, 255, 255), (width+widthTexte/3, y), (width+widthTexte/3, y+50))
+            pygame.draw.line(surface, (255, 255, 255), (width+2*widthTexte/3, y), (width+2*widthTexte/3, y+50))
+            y = y + 50
+
+        for i in range(len(personnage.inventaire)):
+            surface.blit(pygame.image.load('images/canette1.png'), (width, y-50, 50, 50))
+
+    # for l in range(rows):
+    #     x = x + sizeBtwnX
+    #     pygame.draw.line(surface, (0, 0, 0), (x, 0), (x, h))
+    # for L in range(lines):
+    #     y = y + sizeBtwnY
+    #     pygame.draw.line(surface, (0, 0, 0), (0, y), (w, y))
     pass
 
 
@@ -102,7 +131,7 @@ def redrawWindow(surface):
 
 def main():
     pygame.init()
-    global width, height, rows, s, snack, background, lines, soldats, sizeBtwnX, sizeBtwnY, joueur, possibilities, personnage, carte, pas, widthTexte
+    global width, height, rows, s, snack, background, lines, soldats, sizeBtwnX, sizeBtwnY, joueur, possibilities, personnage, carte, pas, widthTexte, listDechet
     pas = 6
 
     carte = [
@@ -127,7 +156,13 @@ def main():
         [1, 1, 0, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 1, 3, 1, 0],
         [1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0],
     ]
-
+    listDechet = []
+    for i in range(len(carte)):
+        for j in range(len(carte[0])):
+            if carte[j][i] == 0:
+                etatevenement = random.randrange(0, 10 , 1)
+                if etatevenement == 1:
+                    listDechet.append((j,i))
 
 
     personnage = Personnage()
@@ -189,6 +224,10 @@ def main():
             y = int((personnage.y+personnage.height)/50)
             if carte[x][y] == 0 or carte[x][y] == 3:
                 personnage.move_left()
+        if pressed.get(pygame.K_SPACE):
+            if personnage.case in listDechet:
+                personnage.inventaire.append((personnage.case[0], personnage.case[1], 1))
+                listDechet.remove(personnage.case)
 
         clock.tick(40)
         redrawWindow(win)
